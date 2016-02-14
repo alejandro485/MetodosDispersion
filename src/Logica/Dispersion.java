4fa57b9cda50686d306c952b92d2718df55be50e
alejandroacr <alejandro@axoneh.com>
2016-02-14 18:35:07 -0500

    Primer commit

---
+++ src/Logica/Dispersion.java
@@ -0,0 +1,72 @@
+package Logica;
+
+public class Dispersion {
+	
+	public String midSquare(int numero, int bits){
+		int d=numero*numero;
+		if(bits<2){
+			return "No se puede operar";
+		}
+		String bin=Integer.toBinaryString(d);
+		int l1=((bin.length()-bits+1)/2);
+		int l2=((bin.length()+bits+1)/2);
+		bin=bin.substring(l1, l2);
+		return Integer.parseInt(bin, 2)+"";
+	}
+	
+	public String division(int numero, int primo){
+		if(isPrimo(primo)){
+			return (numero%primo)+"";
+		}
+		else{
+			return "No se puede operar";
+		}
+	}
+	
+	public String tLlaves(int numero, int primo){
+		return " --";
+	}
+	
+	public String plegamiento(int numero, int separacion){
+		String bin=Integer.toBinaryString(numero);
+		String sum="";
+		for(int i=0; i<separacion;i++){
+			sum+="0";
+		}
+		String ayuda="";
+		while(bin.length()>=separacion){
+			ayuda=bin.substring(bin.length()-separacion, bin.length());
+			sum=suma(sum, ayuda);
+			bin=bin.substring(0, bin.length()-separacion);
+		}
+		for(int i=bin.length(); i<separacion;i++){
+			bin="0"+bin;
+		}
+		sum=suma(sum, bin);
+		return Integer.parseInt(sum,2)+"";
+	}
+	
+	private boolean isPrimo(int num){
+		int raiz=(int)Math.sqrt(num);
+		for(int i=2;i<=raiz;i++){
+			if(num%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private String suma(String v1, String v2){
+		String suma="";
+		for(int i=0;i<v1.length();i++){
+			if(v1.charAt(i)!=v2.charAt(i)){
+				suma+="1";
+			}
+			else{
+				suma+="0";
+			}
+		}
+		return suma;
+	}
+	
+}
---
+++ src/Vista/PantallaEntrada.java
@@ -0,0 +1,121 @@
+package Vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+import Logica.Dispersion;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PantallaEntrada extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Dispersion dispersion;
+	
+	private JPanel contentPane;
+	private JTextField txtNumero;
+	private JTextField txtDato;
+	
+	private JLabel lblNumero;
+	private JLabel lblCodigoDeDispersion;
+	private JLabel lblDatoAuxiliar;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaEntrada frame = new PantallaEntrada();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public PantallaEntrada() {
+		
+		dispersion=new Dispersion();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 453, 250);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtNumero = new JTextField();
+		txtNumero.setBounds(144, 25, 288, 24);
+		contentPane.add(txtNumero);
+		txtNumero.setColumns(10);
+		
+		lblNumero = new JLabel("Numero:");
+		lblNumero.setBounds(12, 29, 70, 15);
+		contentPane.add(lblNumero);
+		
+		lblCodigoDeDispersion = new JLabel("Codigo de dispersion:");
+		lblCodigoDeDispersion.setBounds(12, 100, 421, 15);
+		contentPane.add(lblCodigoDeDispersion);
+		
+		lblDatoAuxiliar = new JLabel("Dato auxiliar");
+		lblDatoAuxiliar.setBounds(12, 65, 116, 15);
+		contentPane.add(lblDatoAuxiliar);
+		
+		txtDato = new JTextField();
+		txtDato.setColumns(10);
+		txtDato.setBounds(144, 61, 288, 24);
+		contentPane.add(txtDato);
+		
+		JButton btnPlegamiento = new JButton("Plegamiento");
+		btnPlegamiento.addActionListener(this);
+		btnPlegamiento.setActionCommand("p");
+		btnPlegamiento.setBounds(22, 127, 193, 25);
+		contentPane.add(btnPlegamiento);
+		
+		JButton btnMidSquare = new JButton("Mid Square");
+		btnMidSquare.addActionListener(this);
+		btnMidSquare.setActionCommand("m");
+		btnMidSquare.setBounds(239, 127, 193, 25);
+		contentPane.add(btnMidSquare);
+		
+		JButton btnDivision = new JButton("Division");
+		btnDivision.addActionListener(this);
+		btnDivision.setActionCommand("d");
+		btnDivision.setBounds(22, 164, 193, 25);
+		contentPane.add(btnDivision);
+		
+		JButton btnLlaves = new JButton("T. de llaves");
+		btnLlaves.addActionListener(this);
+		btnLlaves.setActionCommand("t");
+		btnLlaves.setBounds(239, 164, 193, 25);
+		contentPane.add(btnLlaves);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String r="Codigo de dispersion: ";
+		switch(e.getActionCommand()){
+			case "p":
+				r+=dispersion.plegamiento(Integer.parseInt(txtNumero.getText()),Integer.parseInt(txtDato.getText()));
+				break;
+			case "m":
+				r+=dispersion.midSquare(Integer.parseInt(txtNumero.getText()),Integer.parseInt(txtDato.getText()));
+				break;
+			case "d":
+				r+=dispersion.division(Integer.parseInt(txtNumero.getText()),Integer.parseInt(txtDato.getText()));
+				break;
+			case "t":
+				r+=dispersion.tLlaves(Integer.parseInt(txtNumero.getText()),Integer.parseInt(txtDato.getText()));
+				break;
+		}
+		lblCodigoDeDispersion.setText(r);
+	}
+}
