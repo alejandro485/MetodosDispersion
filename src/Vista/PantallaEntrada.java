a31589dadde2bda6473e08e358264936d2f01c99
alejandroacr <alejandro@axoneh.com>
2016-02-16 21:54:48 -0500

    lawea

--- src/Vista/PantallaEntrada.java
+++ src/Vista/PantallaEntrada.java
@@ -46,7 +46,7 @@ public class PantallaEntrada extends JFrame implements ActionListener{
 		dispersion=new Dispersion();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 453, 250);
+		setBounds(100, 100, 453, 231);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
